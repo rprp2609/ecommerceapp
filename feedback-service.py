96a8dfd80c4885bdc9f43dbb9406699414a73c4c
rprp2609 <rprp2609@gmail.com>
2026-09-22 21:50:48 +0530

    Add vulnerable example of command injection
    Added a vulnerable example of direct string formatting with user input that can lead to command injection.

---
+++ feedback-service.py
@@ -0,0 +1,4 @@
+# VULNERABLE: Direct string formatting with user input
+user_input = input("Enter a parameter: ")
+command = f"echo Printing {user_input}"
+os.system(command)  # If user inputs: "Hello; rm -rf /", it runs the rm command!
