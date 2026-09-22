d5c5b1306da2b5436986688413420505346ce87f
rprp2609 <rprp2609@gmail.com>
2026-09-22 21:42:13 +0530

    Delete cart-service.py

--- cart-service.py
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/login', (req, res) => {
-    const targetUrl = req.query.target;
-    
-    // DANGEROUS: Redirects to any external URL provided by the user
-    res.redirect(targetUrl); 
-});
